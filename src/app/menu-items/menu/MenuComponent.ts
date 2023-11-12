2988adc083bba50dd290578d2ed5b87d2c4b7de1
BeshoySaeed <said.beshoy@yahoo.com>
2023-11-12 13:25:18 -0500

    d

--- src/app/menu-items/menu/MenuComponent.ts
+++ src/app/menu-items/menu/MenuComponent.ts
@@ -10,36 +10,43 @@ export class MenuComponent {
   constructor() {
     this.items = [
       {
+        img: '../../../assets/images/food-gallery/f-3.jpeg',
         name: 'chicken',
         description: '3 pieces',
         price: 200,
       },
       {
+        img: '../../../assets/images/food-gallery/f-5.jpeg',
         name: 'beef',
         description: '2 pieces',
         price: 100,
       },
       {
+        img: '../../../assets/images/food-gallery/f-6.jpeg',
         name: 'meat',
         description: '6 pieces',
         price: 500,
       },
       {
+        img: '../../../assets/images/food-gallery/ff.jpeg',
         name: 'ball roll',
         description: '3 pieces',
         price: 200,
       },
       {
+        img: '../../../assets/images/food-gallery/fff.jpeg',
         name: 'hamburger',
         description: '3 pieces',
         price: 607,
       },
       {
+        img: '../../../assets/images/food-gallery/ffff.jpeg',
         name: 'chicken',
         description: '3 pieces',
         price: 200,
       },
       {
+        img: '../../../assets/images/food-gallery/f-3.jpeg',
         name: 'chicken',
         description: '3 pieces',
         price: 200,
